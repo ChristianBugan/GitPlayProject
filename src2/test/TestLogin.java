5a67dfa77eea423cf70cc7828d007da4286edc00
unknown <christian.bugan@gmail.com>
2023-09-07 09:29:41 +0300

    commit intermediar master

--- src2/test/TestLogin.java
+++ src2/test/TestLogin.java
@@ -3,7 +3,7 @@ package test;
 public class TestLogin {
 	
 	//code added on branch feature.
-
+    // more code here
 	//code added on master branch
 	// some other code added here on master branch
 	
